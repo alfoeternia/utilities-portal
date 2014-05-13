93c118dbbcd196077d83cddbbba60c02b7b9354b
Alexis Fouché <alfo.agonia@gmail.com>
2014-05-13 16:58:31 -0700

    Added basic API

--- UtilityPortal/App_Start/WebApiConfig.vb
+++ UtilityPortal/App_Start/WebApiConfig.vb
@@ -4,6 +4,7 @@ Imports System.Linq
 Imports System.Web.Http
 
 Public Module WebApiConfig
+
     Public Sub Register(ByVal config As HttpConfiguration)
         ' Web API configuration and services
 
@@ -12,8 +13,12 @@ Public Module WebApiConfig
 
         config.Routes.MapHttpRoute(
             name:="DefaultApi",
-            routeTemplate:="api/{controller}/{id}",
-            defaults:=New With {.id = RouteParameter.Optional}
+            routeTemplate:="gw/{controller}/{service}/{type}/{layer}/{queryType}",
+            defaults:=New With {.queryType = RouteParameter.Optional}
         )
+
+        Dim appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(Function(t) t.MediaType = "application/xml")
+        config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType)
+
     End Sub
 End Module
--- UtilityPortal/Scripts/main.js
+++ UtilityPortal/Scripts/main.js
@@ -19,7 +19,7 @@ $.get("/api/layers", function(layers) {
   for (var key in layers) {
 
       // Generate the layer object
-      var layer = L.esri.featureLayer("http://localhost:3000/gw/arcgis/" + layers[key]["service"] + "/" + layers[key]["type"] + "/" + layers[key]["layer"], {
+      var layer = L.esri.featureLayer("/gw/arcgis/" + layers[key]["service"] + "/" + layers[key]["type"] + "/" + layers[key]["layer"], {
         style: function (feature) {
           return { color: layers[key]["color"], weight: 4, opacity: 0.3 };
         },
